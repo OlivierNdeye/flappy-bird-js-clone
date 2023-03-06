a9bb6861ce5d75d0849a2f8f313221d5ca1d7727
Olivier Ndeye <91287614+OlivierNdeye@users.noreply.github.com>
2023-03-06 19:28:07 -0300

    Update do arquivo javascript

--- jogo.js
+++ jogo.js
@@ -73,6 +73,171 @@ function fazColisao(){
 	return false;
 }
 
+		const flappybird = {
+		spriteX: 0,
+		spriteY: 0,
+		largura: 33,
+		altura: 24,
+		x: 10,
+		y: 50,
+		pulo: 4.6,
+		gravidade: 0.25,
+		velocidade: 0,
+		
+	// Declaração de funções	
+		pula(){
+			console.log('Voa Canarinho');
+			console.log('[antes]', flappybird.velocidade);
+			flappybird.velocidade =  - flappybird.pulo;
+			console.log('[depois]', flappybird.velocidade)
+		},
+		atualiza(){		
+			if(fazColisao(flappybird, chao)){
+				console.log("Fez colisão");
+				
+				
+				mudaParaTela(Telas.INICIO);
+				return;
+			}
+			
+			flappybird.velocidade = flappybird.velocidade + flappybird.gravidade;
+			flappybird.y = flappybird.y + flappybird.velocidade;
+		},
+		desenha(){
+			contexto.drawImage(
+				sprites,
+				flappybird.spriteX, flappybird.spriteY, //sprites X e Y
+				flappybird.largura, flappybird.altura, // Tamanho do Recorte de X e Y
+				flappybird.x, flappybird.y,
+				flappybird.largura, flappybird.altura,
+			);
+		}
+	}
+
+
+//[mesagem de GetReady]
+const mensagemGetReady = {
+	sX: 134,
+	sY: 0,
+	w:  174,
+	h:  152,
+	x:  (canvas.width / 2) - 174 /2,
+	y:  50,
+	desenha(){
+		contexto.drawImage(
+			sprites,
+			mensagemGetReady.sX, mensagemGetReady.sY,
+			mensagemGetReady.w, mensagemGetReady.h,
+			mensagemGetReady.x, mensagemGetReady.y,
+			mensagemGetReady.w, mensagemGetReady.h
+		);
+	}
+}
+
+//
+// As telas do Jogo e troca entre telas
+//
+let telaAtiva = {};
+function mudaParaTela(novaTela){
+	telaAtiva = novaTela;
+	
+/* 	if(telaAtiva.inicializa) {
+		inicializa();
+	} */
+
+}
+
+const Telas = {
+	INICIO:{
+
+		desenha(){
+			planoDeFundo.desenha();
+			chao.desenha();
+			flappybird.desenha();			
+			mensagemGetReady.desenha();
+		},
+		click(){
+			mudaParaTela(Telas.JOGO)
+		},
+		atualiza(){
+			
+		}
+	}
+};
+
+
+Telas.JOGO = {
+	desenha(){
+		planoDeFundo.desenha();
+		chao.desenha();
+		flappybird.desenha();
+	},
+	click(){
+		flappybird.pula();
+	},
+	atualiza(){
+		flappybird.atualiza();
+	}
+};
+
+
+function loop() {
+	
+	telaAtiva.desenha();
+	telaAtiva.atualiza();
+	
+	requestAnimationFrame(loop);
+}
+
+window.addEventListener('click', function() {
+	if(telaAtiva.click) {
+		telaAtiva.click();
+	}
+});
+
+mudaParaTela(Telas.INICIO);
+loop();
+
+// Backgroud do Jogo
+const planoDeFundo = {
+	spriteX: 390,
+	spriteY: 0,
+	largura: 275,
+	altura: 204,
+	x: 0,
+	y: canvas.height - 204,
+	desenha() {
+		contexto.fillStyle = '#70c5ce';
+		contexto.fillRect(0, 0, canvas.width, canvas.height);
+		
+		contexto.drawImage(
+			sprites,
+			planoDeFundo.spriteX, planoDeFundo.spriteY, //sprites X e Y
+			planoDeFundo.largura, planoDeFundo.altura, // Tamanho do Recorte de X e Y
+			planoDeFundo.x, planoDeFundo.y,
+			planoDeFundo.largura, planoDeFundo.altura,
+		);
+		
+		contexto.drawImage(
+			sprites,
+			planoDeFundo.spriteX, planoDeFundo.spriteY, //sprites X e Y
+			planoDeFundo.largura, planoDeFundo.altura, // Tamanho do Recorte de X e Y
+			(planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
+			planoDeFundo.largura, planoDeFundo.altura,
+		);
+	}
+}
+
+function fazColisao(){
+	const flappybirdY = flappybird.y + flappybird.altura;
+	const chaoY = chao.y;
+	
+	if(flappybirdY >= chaoY) {
+		return true;
+	}
+	return false;
+}
+
 function criaFlappybird(){
 		const flappybird = {
 		spriteX: 0,
